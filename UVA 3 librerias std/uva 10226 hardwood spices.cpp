f07d54422a6a3e6ea31c862a68ee0661a24959ac
Unknown <mdelcarpiol@ulasalle.edu.pe>
2019-06-12 09:40:16 -0500

    refresh 2

---
+++ UVA 3 librerias std/uva 10226 hardwood spices.cpp
@@ -0,0 +1,32 @@
+//Marvik
+//Guillermo
+#include <iostream>
+#include <iomanip>
+#include <map>
+using namespace std;
+int main() {
+	int t;
+	cin>>t;
+	getchar();
+    getchar();
+	while(t--) {
+		map<string, int> trees;
+		string tree;
+		int n = 0;
+		while(getline(cin, tree)) {
+			if(tree.compare("") == 0)
+                {
+                    break;
+                }
+			trees[tree]++;
+			n++;
+		}
+		for(auto i: trees)
+        {
+            cout << i.first << " " << fixed << setprecision(4) << (i.second*100.0)/n << endl;
+
+        }
+        if(t>0)cout<<"\n";
+	}
+    return 0;
+}
---
+++ UVA 3 librerias std/uva 11572 Unique Snowflakes.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+//Marvik
+//Guillermo
+#include <set>
+#include <assert.h>
+
+using namespace std;
+int main() {
+
+    int t,n,j,r;
+    cin>>t;
+    while(t--)
+    {
+        n=0;
+        r=0;
+        cin>>n;
+        assert(n<109);
+        set<int,greater<int> >snowflakes;
+        for(int i =0; i< n; i++)
+        {
+            cin>>j;
+            snowflakes.insert(j);
+        }
+        for(auto i:snowflakes)
+        {
+            r++;
+        }
+        cout<<endl<<r<<endl;
+
+    }
+
+    return 0;
+}
